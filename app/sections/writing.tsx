46bc053692a70e2d29958b9cdf8040bc0dc3bda3
Gareth MacLeod <gareth.macleod@gmail.com>
2024-03-28 17:28:41 -0400

    Make the blog preview images clickable

--- app/sections/writing.tsx
+++ app/sections/writing.tsx
@@ -36,7 +36,9 @@ export default function Writing() {
                   alt=""
                   className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
                 />
-                <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+                <a href={post.href}>
+                  <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+                </a>
               </div>
               <div className="max-w-xl">
                 <div className="mt-8 flex items-center gap-x-4 text-xs">
