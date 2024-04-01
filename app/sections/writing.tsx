bdc9c0d4f544c77681949d738dc1eba586d14acc
Gareth MacLeod <gareth.macleod@gmail.com>
2024-04-01 17:58:56 -0400

    Moved the list of posts into a file in the /data directory.

--- app/sections/writing.tsx
+++ app/sections/writing.tsx
@@ -1,24 +1,4 @@
-const posts = [
-  {
-    id: 1,
-    title: "How to feel when your startup feels easy",
-    href: "https://garethdmm.medium.com/how-to-feel-when-your-startup-feels-easy-1c40b0b4210a",
-    description:
-      "I did something hard once: I took my startup from zero to $1m in daily volume in 4 months. Something has bothered me ever since: it felt easy.",
-    imageUrl: "/ease_post.webp",
-    date: "Feb 16, 2024",
-    datetime: "2024-02-16",
-  },
-  {
-    id: 2,
-    title: "How we survived 5 years in the most dangerous market in the world.",
-    href: "https://garethdmm.medium.com/how-we-survived-5-years-in-the-most-dangerous-market-in-the-world-c1404e0ab5b9",
-    description: "War stories from the early crypto days.",
-    imageUrl: "/risk.png",
-    date: "Aug 26, 2019",
-    datetime: "2019-08-26",
-  },
-];
+import { writing_data } from "@/data/writing";
 
 export default function Writing() {
   return (
@@ -28,7 +8,7 @@ export default function Writing() {
           Here&apos;s some of my writing.
         </h2>
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 sm:grid-cols-2 lg:mx-0">
-          {posts.map((post) => (
+          {writing_data.map((post) => (
             <article key={post.id} className="flex flex-col items-start">
               <div className="relative w-full">
                 <img
---
+++ data/writing.tsx
@@ -0,0 +1,21 @@
+export const writing_data = [
+  {
+    id: 1,
+    title: "How to feel when your startup feels easy",
+    href: "https://garethdmm.medium.com/how-to-feel-when-your-startup-feels-easy-1c40b0b4210a",
+    description:
+      "I did something hard once: I took my startup from zero to $1m in daily volume in 4 months. Something has bothered me ever since: it felt easy.",
+    imageUrl: "/ease_post.webp",
+    date: "Feb 16, 2024",
+    datetime: "2024-02-16",
+  },
+  {
+    id: 2,
+    title: "How we survived 5 years in the most dangerous market in the world.",
+    href: "https://garethdmm.medium.com/how-we-survived-5-years-in-the-most-dangerous-market-in-the-world-c1404e0ab5b9",
+    description: "War stories from the early crypto days.",
+    imageUrl: "/risk.png",
+    date: "Aug 26, 2019",
+    datetime: "2019-08-26",
+  },
+]
