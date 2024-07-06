b36d3f6e344fc822da31577962c80e7a8b320340
Gareth MacLeod <gareth.macleod@gmail.com>
2024-07-05 23:54:39 -0400

    Added a footer which is shared between homepage and blog sections.

--- src/app/writing/layout.tsx
+++ src/app/writing/layout.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
+import Footer from "../sections/footer";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col w-full">
+    <div className="flex flex-col w-full min-h-screen">
       <Header />
-      <div className="relative flex w-full flex-col">{children}</div>
+      <div className="relative flex w-full flex-col grow mb-16">{children}</div>
+      <Footer />
     </div>
   );
 }
