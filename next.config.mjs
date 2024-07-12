a9b63581def3c4c51c034922a47f930491c32123
Gareth MacLeod <gareth.macleod@gmail.com>
2024-07-12 16:27:04 -0400

    Downgraded back to nextjs 13 because our timeline component doesn't play nice with next 14. We'll replace that component soon.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone', // mine worked fine without this line
+    experimental: {
+        serverActions: true,
+    },
 };
 
 export default nextConfig;
